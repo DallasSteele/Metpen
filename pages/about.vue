7272254aa84fb55b80e73aca4a9ddd1858d30993
Sheerwood Jack Grave <harleenq658@gmail.com>
2024-07-14 16:22:53 +0700

    Latest
    without an authentication

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container mt-5">
     <h1>About Us!</h1>
   </div>
 </template>
@@ -16,5 +16,7 @@ export default {
 </script>
 
 <style>
-
+ .container {
+  max-width: 800px;
+}
 </style>
